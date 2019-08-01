b1bd4304a0b0d281dff39adb0e843117d50ad8aa
hoangminhhuyen <52224369+hoangminhhuyen@users.noreply.github.com>
2019-08-01 14:22:04 +0700

    Em xin tặng thầy 1 bản bonus có plot ạ!

---
+++ 2. annotated heatmap.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+movies = pd.read_csv('movies.csv')
+
+#tách year ra khỏi title
+movies["year"] = movies["title"].apply(lambda x: x[-5:-1])
+#lọc ra những year NaN
+movies.year=pd.to_numeric(movies['year'],errors='coerce',downcast='integer')
+#print(movies)
+
+# Tao ra cac cot dummy
+movies = movies.join(movies.genres.str.get_dummies())
+
+a = movies.loc[:, "(no genres listed)":"Western"].corr().round(1)
+print(a)
+
+plt.yticks(np.arange(0.5, len(a.index), 1), a.index)
+plt.ylabel("genres")
+plt.xticks(np.arange(0.5, len(a.index), 1), a.index) 
+plt.xlabel("genres")
+plt.pcolor(a)
+
+for i in range(len(a.columns)):
+    for j in range(len(a.index)):
+        plt.text(0.5 + i, 0.5 + j, a.iat[j, i],
+ha="center", va="center", color="r")
+
+plt.show()
